e32af19a05793ddd87277c46fc3aef2a7da700ca
Gert-Jan <gjroskam@on.nl>
2025-12-28 22:17:16 +0100

    Add webserial output

---
+++ src/utils/webserial.cpp
@@ -0,0 +1,34 @@
+//
+// Created by zweepje on 28-12-2025.
+//
+#include <WebSerial.h>
+
+// Functie prototype
+void debugPrintf(const char *format, ...) {
+    char loc_res[256]; // Buffer voor de geformatteerde tekst
+    va_list arg;
+    va_list copy;
+
+    va_start(arg, format);
+    va_copy(copy, arg);
+
+    // Bereken hoe groot de string gaat worden
+    int len = vsnprintf(nullptr, 0, format, copy);
+    va_end(copy);
+
+    if (len > 0) {
+        char *buffer = (char *)malloc(len + 1);
+        if (buffer) {
+            vsnprintf(buffer, len + 1, format, arg);
+
+            // 1. Stuur naar de fysieke Seriële poort
+            Serial.print(buffer);
+
+            // 2. Stuur naar de WebSerial interface
+            WebSerial.print(buffer);
+
+            free(buffer);
+        }
+    }
+    va_end(arg);
+}
---
+++ src/utils/webserial.h
@@ -0,0 +1,10 @@
+//
+// Created by zweepje on 28-12-2025.
+//
+
+#ifndef MYIPIXEL_WEBSERIAL_H
+#define MYIPIXEL_WEBSERIAL_H
+#include <WebSerial.h>
+
+void debugPrintf(const char *format, ...);
+#endif //MYIPIXEL_WEBSERIAL_H
