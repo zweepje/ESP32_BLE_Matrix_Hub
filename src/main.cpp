81e7f8d9785f61c154384902fc72d0aeea75c0a2
Gert-Jan <gjroskam@on.nl>
2026-01-31 12:41:34 +0100

    Version 2.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -266,7 +266,6 @@ void setup() {
   Serial.begin(115200);
   delay(200); // Wacht even op de seriële monitor
 	Serial.println("ESP - BLE matrix HUB");
-	Serial.println("TESTTESTTESTTESTTEST");
 	Serial.println("================================");
 	Serial.printf("Software Versie: %s\n", VERSION);
 	Serial.printf("Build Nummer:    %s\n", BUILD_NUMBER);
